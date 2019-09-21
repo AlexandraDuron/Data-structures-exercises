2bd11e48bb867c91b5a51385c122835d8c5412d6
Alexandra Duron <alexandra.oduron@gmail.com>
2019-09-21 15:39:55 +0100

    Added comments

--- Stack/Test.java
+++ Stack/Test.java
@@ -1,6 +1,8 @@
-/*  #Stack Brackets example
+/*  Stack Brackets example
 
-A string containing only parentheses is balanced if the following is true: 1. if it is an empty string 2. if A and B are correct, AB is correct, 3. if A is correct, (A) and {A} and [A] are also correct.
+A string containing only parentheses is balanced if the following is true: 1. if it is an empty string 2. if A and B are correct, 
+
+AB is correct, 3. if A is correct, (A) and {A} and [A] are also correct.
 
 Examples of some correctly balanced strings are: "{}()", "[{()}]", "({()})"
 
